ad867c27b0c4847c2b66b31563d7d56dd636b395
David Ch <chetodb@gmail.com>
2026-03-10 21:59:55 +0100

    build(core): enhance local development exports and vitest config resolution

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['src/**/*.spec.ts'],
+    exclude: ['**/dist/**', '**/node_modules/**'],
     setupFiles: ['test/setup.ts'],
     silent: true,
     coverage: {
